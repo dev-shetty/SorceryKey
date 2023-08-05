6823b74c55be210a1cbcc3e51ff490c091e40fd7
Deveesh Shetty <deveeshshetty@gmail.com>
2023-08-05 22:26:31 +0530

    Ignores generated JS files

--- dist/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getRandomNumber = void 0;
-function getRandomNumber(range) {
-    return Math.floor(Math.random() * range);
-}
-exports.getRandomNumber = getRandomNumber;
-//# sourceMappingURL=utils.js.map
